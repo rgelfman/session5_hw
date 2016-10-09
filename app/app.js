b528e15d92c8cb3081b4509ea0a0ec0cab7e1512
rgelfman <rgelfman@yahoo.com>
2016-10-08 22:45:19 -0400

    final commit

--- app/app.js
+++ app/app.js
@@ -5,8 +5,8 @@ var dataFile = require('./data/data.json');
 app.set('port', process.env.PORT || 3000 );
 app.set('appData', dataFile);
 
-app.use(express.static('app/public'));
-
+ app.use(express.static('app/public'));
+app.use(express.static('app'));
 var server = app.listen(app.get('port'), function() {
   console.log('Listening on port ' + app.get('port'));
 });
